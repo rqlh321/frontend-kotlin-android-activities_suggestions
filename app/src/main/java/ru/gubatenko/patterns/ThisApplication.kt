62b11ed0bae432e1670e43ef73cb31d0147a1d0a
rqlh321 <rqlh321@gmail.com>
2021-08-30 16:21:58 +0300

    feature/1 add work manager for sync

--- app/src/main/java/ru/gubatenko/patterns/ThisApplication.kt
+++ app/src/main/java/ru/gubatenko/patterns/ThisApplication.kt
@@ -1,16 +1,18 @@
 package ru.gubatenko.patterns
 
 import android.app.Application
+import androidx.work.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import ru.gubatenko.data_impl.daoModuleDI
+import ru.gubatenko.data_impl.dtoMapperImplModuleDI
 import ru.gubatenko.data_impl.serviceImplModuleDI
+import ru.gubatenko.data_impl.storedMapperImplModuleDI
 import ru.gubatenko.domain_impl.repoImplModuleDI
 import ru.gubatenko.domain_impl.usaCaseImplModuleDI
 import ru.gubatenko.feature_main_android.mainFeatureAndroidModuleDI
-import ru.gubatenko.data_impl.daoModuleDI
-import ru.gubatenko.data_impl.dtoMapperImplModuleDI
-import ru.gubatenko.data_impl.storedMapperImplModuleDI
+import java.util.concurrent.TimeUnit
 
 class ThisApplication : Application() {
 
@@ -29,5 +31,21 @@ class ThisApplication : Application() {
                 mainFeatureAndroidModuleDI,
             )
         }
+
+        val constraint = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresBatteryNotLow(true)
+            .build()
+
+        val uploadWorkRequest = PeriodicWorkRequestBuilder<UploadWorker>(5, TimeUnit.SECONDS)
+            .setConstraints(constraint)
+            .build()
+
+        WorkManager.getInstance(this)
+            .enqueueUniquePeriodicWork(
+                "MyAppNameBackgroundSync",
+                ExistingPeriodicWorkPolicy.KEEP,
+                uploadWorkRequest
+            )
     }
 }
