c477eefb0a31c8e47282631a7cedf217fc71c44f
sic <rqlh321@gmail.com>
2021-09-06 21:26:43 +0300

    feature/5 setup subscriptions

--- feature_promises_android/src/main/java/com/example/feature_accepted_activities_android/ActivitiesFragment.kt
+++ feature_promises_android/src/main/java/com/example/feature_accepted_activities_android/ActivitiesFragment.kt
@@ -7,17 +7,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.feature_accepted_activities.AcceptedActivitiesStore
 import com.example.feature_accepted_activities_android.adapter.ActivitiesAdapter
 import com.example.feature_accepted_activities_android.adapter.ActivityModel
-import com.example.navigation.NavigationScope
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.withContext
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.unloadKoinModules
 import ru.gubatenko.common_android.BaseFragment
-import ru.gubatenko.common_android.sharedGraphViewModel
 
 class ActivitiesFragment : BaseFragment(R.layout.fragment_accepted_activities) {
 
-    private val viewModel: ActivitiesViewModel by sharedGraphViewModel(NavigationScope.FRAME_SCOPE)
+    private val viewModel: ActivitiesViewModel by viewModel()
 
     private val list: RecyclerView by lazy { requireView().findViewById(R.id.activities_list_id) }
     private val adapter: ActivitiesAdapter by lazy { ActivitiesAdapter() }
