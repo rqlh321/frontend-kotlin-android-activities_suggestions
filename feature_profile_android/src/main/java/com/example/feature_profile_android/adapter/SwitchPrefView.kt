e73b215829a9bb2971cc70932bf2d3e8f741fbc3
sic <rqlh321@gmail.com>
2021-10-06 23:02:48 +0300

    setup pref list item

--- feature_profile_android/src/main/java/com/example/feature_profile_android/adapter/SwitchPrefView.kt
+++ feature_profile_android/src/main/java/com/example/feature_profile_android/adapter/SwitchPrefView.kt
@@ -2,26 +2,29 @@ package com.example.feature_profile_android.adapter
 
 import android.content.Context
 import android.widget.FrameLayout
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.feature_profile.SwitchPrefModel
 import com.example.feature_profile_android.R
 import com.google.android.material.switchmaterial.SwitchMaterial
 
-class SwitchPrefView(context: Context) : FrameLayout(context) {
+class SwitchPrefView(context: Context) : LinearLayout(context) {
 
     private val title: TextView by lazy { findViewById(R.id.title_id) }
     private val state: SwitchMaterial by lazy { findViewById(R.id.state_id) }
 
     init {
-        layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        layoutParams = LayoutParams(
+            LayoutParams.MATCH_PARENT,
+            context.resources.getDimension(R.dimen.medium_item_height).toInt()
+        )
+        orientation = HORIZONTAL
         inflate(getContext(), R.layout.list_item_pref_switch, this)
     }
 
     fun bind(model: SwitchPrefModel, action: ((String, Boolean) -> Unit)? = null) {
         title.text = model.title
         state.isChecked = model.isOn
-        state.setOnCheckedChangeListener { compoundButton, isChecked ->
-            action?.invoke(model.id, isChecked)
-        }
+        state.setOnCheckedChangeListener { _, isChecked -> action?.invoke(model.id, isChecked) }
     }
 }
